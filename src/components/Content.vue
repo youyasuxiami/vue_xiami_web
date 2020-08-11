833e2bfd1281b989c9cb2adedb8afc78af2b8967
xiami <891627097@qq.com>
2020-08-12 01:14:39 +0800

    初始化项目完成

--- src/components/Content.vue
+++ src/components/Content.vue
@@ -1,12 +1,12 @@
 <template>
-  <div id="app">
-    <router-view/>
+  <div>
+    我是内容页
   </div>
 </template>
 
 <script>
   export default {
-    name: 'App',
+    name: "Content"
   }
 </script>
 
--- src/main.js
+++ src/main.js
@@ -1,13 +1,27 @@
-// The Vue build version to load with the `import` command
-// (runtime-only or standalone) has been set in webpack.base.conf with an alias.
 import Vue from 'vue'
+
+// 导入路由库
+import VueRouter from 'vue-router'
+
+// 导入路由文件夹
+import router from './router'
+
+// 导入 ElementUI库
+import ElementUI from 'element-ui'
+import 'element-ui/lib/theme-chalk/index.css'
+
 import App from './App'
 
-Vue.config.productionTip = false
+// 安装路由
+Vue.use(VueRouter);
 
-/* eslint-disable no-new */
+// 安装 ElementUI
+Vue.use(ElementUI);
+
 new Vue({
   el: '#app',
-  components: { App },
-  template: '<App/>'
-})
+  // 启用路由
+  router,
+  // 启用 ElementUI
+  render: h => h(App)
+});
